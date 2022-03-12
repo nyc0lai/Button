20acaa8e1b6f7a79137faeb7e2c970e80057b2f2
nyc0lai <nyc0lai@gmail.com>
2022-03-12 10:45:43 +0200

    add console.clear() method

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 new Button("CLICK COUNT")
     .mount(document.querySelector("#button"), function () {
+            console.clear()
             this.count++
             console.log(this.count)
         }
